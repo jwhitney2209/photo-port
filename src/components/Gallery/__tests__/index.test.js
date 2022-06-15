a2474c2ea58a09c8436a8e64203670df4414f1c6
teachergg <jwhitney@bisd.us>
2022-06-15 18:00:19 -0500

    added contact form elements in JSX

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -1,13 +1,14 @@
+// __tests__/Gallery.test.js
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Gallery from '..'
-
-const portrait = { name: "portraits", description: "Portraits of people in my life"};
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
 
 afterEach(cleanup)
 
-describe('Gallery is rendering', () => {
+describe('Gallery component', () => {
+
   it('renders', () => {
     render(<Gallery currentCategory={portrait} />);
   });
@@ -15,11 +16,10 @@ describe('Gallery is rendering', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Gallery currentCategory={portrait} />)
     expect(asFragment()).toMatchSnapshot()
-  });
-
-  // eslint-disable-next-line jest/no-identical-title
-  it('renders', () => {
-    const { getByTestId } = render(<Gallery currentCategory={portrait} />)
-    expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
   })
+});
+
+it('renders', () => {
+  const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
 })
